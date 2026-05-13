11cea2f5da8a9a2f9f1c2a4d298803368853b802
alexey <lesal2767@gmail.com>
2026-05-13 14:53:21 +0300

    Task 1: The HTTP requests script has been done

--- task_1/main.py
+++ task_1/main.py
@@ -1,5 +1,41 @@
+import logging
+
+import requests
+
+
+# Basic HTTP exception
+class HTTPStatusException(Exception):
+    pass
+
+
+# Logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+def request(url: str) -> requests.Response:
+    response = requests.get(url, allow_redirects=False)
+    status = response.status_code
+    if status < 400:
+        logging.info(f"Success response, status code: {status}")
+        return response
+    else:
+        raise HTTPStatusException(f"Received an error status code: {status}")
+
+
 def main():
-    print("Hello from task-1!")
+    base_url = "https://tools-httpstatus.pickup-services.com"
+
+    endpoints = ["/101", "/200", "/301", "/408", "/502"]
+    for endpoint in endpoints:
+        try:
+            request(base_url + endpoint)
+        except HTTPStatusException as e:
+            logging.error(e)
+        except requests.exceptions.RequestException as e:
+            # Catching network errors
+            logging.error(f"Network error on {endpoint}: {e}")
 
 
 if __name__ == "__main__":
